bdf5ae2107ed1ce332610b69d406ff67a158971a
rzdzaky <76608040+rzdzaky@users.noreply.github.com>
2021-09-19 19:43:40 +0700

    membuat top wrapper

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -6,13 +6,13 @@ const MainHeader = () => {
     <header>
       <div class="container">
         <div class="header-left">
-          <h3>RZDZAKY</h3>
+          <h3>Code With RZDZAKY</h3>
         </div>
         <span class="fa fa-bars menu-icon"></span>
         <div class="header-right">
-          <a href="#">About Me</a>
-          <a href="#">Projects</a>
-          <a href="#" class="left">Contact</a>
+          <a href="#">Pelajaran</a>
+          <a href="#">Daftar</a>
+          <a href="#" class="left">Log in</a>
         </div>
       </div>
     </header>
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StatefullComponent from './containers/StatefullComponent';
 import MainHeader from './components/MainHeader/MainHeader';
+import TopWrapper from './components/TopWrapper/TopWrapper';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <StatefullComponent /> */}
     <MainHeader />
+    <TopWrapper />
   </React.StrictMode>,
-  document.getElementById('header')
+  document.getElementById('MainHeader')
 );
 
 // If you want to start measuring performance in your app, pass a function
